0bc972f476112c3fd142e3bc9274f528d3448124
Siddant <siddantgurung@hotmail.co.uk>
2019-02-26 19:05:22 +0000

    added the id on the comment table

--- models/comment.py
+++ models/comment.py
@@ -1,5 +1,6 @@
 from app import db, ma
 from .base import BaseModel, BaseSchema
+from marshmallow import fields
 
 class Comment(db.Model, BaseModel):
 
@@ -7,9 +8,13 @@ class Comment(db.Model, BaseModel):
 
     text = db.Column(db.Text(1000), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
-    usere = db.relationship('User', backref='stories')
+    user = db.relationship('User', backref='user_comment')
+    story_id = db.Column(db.Integer, db.ForeignKey('story.id'))
+    story = db.relationship('Story', backref='comment')
 
 class MessageSchema(ma.ModelSchema, BaseSchema):
+    users = fields.Nested('UserSchema')
+    creator = fields.Nested('UserSchema')
 
     class Meta:
         model = Comment
