abf596f535558af7c4e53b6e7ebba12cdfd3c034
Siddant <siddantgurung@hotmail.co.uk>
2019-02-26 20:48:19 +0000

    done the reading list model

--- models/readinglist.py
+++ models/readinglist.py
@@ -5,6 +5,9 @@ from .base import BaseModel, BaseSchema
 class ReadingList(db.Model, BaseModel):
 
     __tablename__ = 'readinglists'
+    title = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    user = db.relationship('User', backref='readinglists')
 
 
 class ReadingListSchema(ma.ModelSchema, BaseSchema):
