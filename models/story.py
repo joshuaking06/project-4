814a09c7bc16114e0258bdc30774f400ed5e3753
joshuaking06 <joshua.king0692@gmail.com>
2019-02-26 17:09:58 +0000

    added genre to story model

--- models/story.py
+++ models/story.py
@@ -6,6 +6,7 @@ class Story(db.Model, BaseModel):
     title = db.Column(db.String(255), nullable=False)
     description = db.Column(db.String(255), nullable=False)
     content = db.Column(db.Text(), nullable=False)
+    genre = db.Column(db.String(30), nullable=False)
 
 class StorySchema(ma.ModelSchema, BaseSchema):
     class Meta:
