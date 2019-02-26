e09be377d08d925283c29900ef6116de5c2ca287
Alessandro Lepri <alex.lepri84@gmail.com>
2019-02-26 20:30:44 +0000

    Finalised auth.py

--- controllers/auth.py
+++ controllers/auth.py
@@ -7,8 +7,7 @@ api = Blueprint('auth', __name__)
 user_schema = UserSchema()
 message_schema = MessageSchema()
 
-@api.route('/register', methods=['GET'])
-# @secure_route
+@api.route('/register', methods=['POST'])
 def register():
 
     user, errors = user_schema.load(request.get_json())
@@ -18,7 +17,7 @@ def register():
 
     user.save()
 
-    return jsonify({'message': 'Registration complete'}), 201
+    return jsonify({'message': 'Registration successful'}), 201
 
 
 @api.route('/login', methods=['POST'])
@@ -34,3 +33,31 @@ def login():
         'message': 'Welcome back {}!'.format(user.username),
         'token': user.generate_token()
     })
+
+@api.route('/users/<int:user_id>', methods=['GET'])
+def users_show(user_id):
+    user = User.query.get(user_id)
+    return user_schema.jsonify(user)
+
+
+@api.route('/users/<int:user_id>/inbox', methods=['POST'])
+@secure_route
+def send_message(user_id):
+
+    message, errors = message_schema.load(request.get_json())
+    message.sender = g.current_user
+    message.receiver = User.query.get(user_id)
+
+    if errors:
+        return jsonify(errors), 422
+
+    message.save()
+    return message_schema.jsonify(message)
+
+
+
+@api.route('/me', methods=['GET'])
+@secure_route
+def me():
+
+    return user_schema.jsonify(g.current_user)
