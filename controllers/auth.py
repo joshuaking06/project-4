52a77a049022c4e49be9f6c4584790842b9d48e0
Alessandro Lepri <alex.lepri84@gmail.com>
2019-02-26 17:38:21 +0000

    Added register and login route in auth

--- controllers/auth.py
+++ controllers/auth.py
@@ -11,4 +11,26 @@ message_schema = MessageSchema()
 # @secure_route
 def register():
 
-    user, errors = user_schema.load
+    user, errors = user_schema.load(request.get_json())
+
+    if errors:
+        return jsonify(errors), 422
+
+    user.save()
+
+    return jsonify({'message': 'Registration complete'}), 201
+
+
+@api.route('/login', methods=['POST'])
+def login():
+
+    data = request.get_json()
+    user = User.query.filter_by(email=data.get('email')).first()
+
+    if not user or not user.validate_password(data.get('password', '')):
+        return jsonify({'message': 'Unauthorized'}), 401
+
+    return jsonify({
+        'message': 'Welcome back {}!'.format(user.username),
+        'token': user.generate_token()
+    })
