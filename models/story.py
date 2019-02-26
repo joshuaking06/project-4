8fec2e3f4e7ea960b72091bbc95987ab3ab9eaba
joshuaking06 <joshua.king0692@gmail.com>
2019-02-26 17:12:07 +0000

    added tablename

--- models/story.py
+++ models/story.py
@@ -3,6 +3,9 @@ from marshmallow import fields
 from .base import BaseModel, BaseSchema
 
 class Story(db.Model, BaseModel):
+    
+    __tablename__ = 'stories'
+
     title = db.Column(db.String(255), nullable=False)
     description = db.Column(db.String(255), nullable=False)
     content = db.Column(db.Text(), nullable=False)
