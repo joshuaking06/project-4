2022c09e49fa382d16a1c7deba03e9dceddc0222
Siddant <siddantgurung@hotmail.co.uk>
2019-02-26 17:27:22 +0000

    starting on the seeds file

---
+++ seeds.py
@@ -0,0 +1,12 @@
+from app import db, app
+from models.user import User
+from models.story import Story
+from models.message import Message
+from models.comment import Comment
+
+
+with app.app_context():
+    db.drop_all()
+    db.create_all()
+
+    alessandro = 
