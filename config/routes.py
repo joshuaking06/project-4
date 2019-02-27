1a7e3774ea1194e0e5251a559417149de07dbb0b
Alessandro Lepri <alex.lepri84@gmail.com>
2019-02-27 14:08:48 +0000

    typos

--- config/routes.py
+++ config/routes.py
@@ -1,13 +1,6 @@
 from app import app
-<<<<<<< HEAD
-from controllers import auth, stories, reddit
-
-app.register_blueprint(auth.api, url_prefix='/api')
-app.register_blueprint(stories.api, url_prefix='/api')
-=======
 from controllers import auth, readinglists, reddit
 
 app.register_blueprint(auth.api, url_prefix='/api')
 app.register_blueprint(readinglists.api, url_prefix='/api')
->>>>>>> development
 app.register_blueprint(reddit.api, url_prefix='/api')
