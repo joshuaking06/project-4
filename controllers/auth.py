bad0c009b111029a0e7e86fa05dc2f55e368d7de
joshuaking06 <joshua.king0692@gmail.com>
2019-02-27 14:59:21 +0000

    added delete route on readinlist

--- controllers/auth.py
+++ controllers/auth.py
@@ -122,12 +122,17 @@ def add_new(user_id, story_id):
 
     user.save()
 
-
     return user_schema.jsonify(user)
+
+
 
 @api.route('/users/<int:user_id>/stories/<int:story_id>', methods=['DELETE'])
 def delete_from_reading_list(user_id, story_id):
     user = User.query.get(user_id)
-    print(user.read_list)
+    story = Story.query.get(story_id)
 
-    return 'hello there', 204
+    user.read_list.remove(story)
+
+    user.save()
+
+    return '', 204
