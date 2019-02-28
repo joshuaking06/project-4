547d43ae51feb12f17186ff72d4086d0d0a6b4ac
Siddant <siddantgurung@hotmail.co.uk>
2019-02-28 10:41:07 +0000

    half way with the users layout

--- src/components/user/UsersShow.js
+++ src/components/user/UsersShow.js
@@ -1,6 +1,11 @@
 import React from 'react'
-import { Segment, Container, Grid, Image  } from 'semantic-ui-react'
+import { Segment,Statistic, Container, Grid, Image,Header,Divider  } from 'semantic-ui-react'
 
+const items = [
+  { key: 'followers', label: 'Followers', value: '22' },
+  { key: 'followings', label: 'Followings', value: '31,200' },
+  { key: 'posts', label: 'Posts', value: '22' }
+]
 
 class UsersShow extends React.Component{
 
@@ -9,12 +14,24 @@ class UsersShow extends React.Component{
   render(){
     return(
       <Container>
-        <Grid columns={1} stackable textAlign='center'>
-          <Grid.Column width={8} >
-            <Image src='https://react.semantic-ui.com/images/avatar/large/patrick.png' size='medium' circular centered/>
-          </Grid.Column>
-        </Grid>
+        <Segment>
 
+          <Grid columns={1} stackable textAlign='center'>
+            <Grid.Column width={8}  >
+              <Header as='h1' icon textAlign='center'>
+                <Header.Content>User name</Header.Content>
+              </Header>
+              <Image src='https://react.semantic-ui.com/images/avatar/large/patrick.png' size='medium' circular centered/>
+              <Divider section hidden />
+              <Grid columns={3} stackable textAlign='center'>
+                <Statistic label='followers' value='5,550' />
+                <Statistic label='followings' value='5,550' />
+                <Statistic label='posts' value='5,550' />
+              </Grid>
+
+            </Grid.Column>
+          </Grid>
+        </Segment>
       </Container>
 
     )
