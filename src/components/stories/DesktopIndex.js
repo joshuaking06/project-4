c7f71d9181b0379cbb563fa8e2083272e04bb427
joshuaking06 <joshua.king0692@gmail.com>
2019-02-28 16:02:17 +0000

    set up links on desktop index

--- src/components/stories/DesktopIndex.js
+++ src/components/stories/DesktopIndex.js
@@ -1,24 +1,45 @@
 import React from 'react'
-import { Container, Segment } from 'semantic-ui-react'
+import { Link } from 'react-router-dom'
+import { Container, Segment, Grid } from 'semantic-ui-react'
+
+const style = {
+  height: '400px',
+  backgroundImage: 'url(https://marketplace.canva.com/MABdzJjyLYc/1/thumbnail_large/canva-student-writing-paper-lined--MABdzJjyLYc.png)',
+  backgroundSize: 'cover'
+}
 
 class DesktopIndex extends React.Component{
   constructor(props){
     super(props)
-
-    this.state={
-      stories: this.props.stories
-    }
   }
 
 
   render(){
     return(
       <Container>
-        <Segment raised>
-          {this.state.stories.map(story =>
-            <div key={story.id}>
-          )}
-        </Segment>
+        <Grid columns={3}>
+          <Grid.Column width={3}></Grid.Column>
+          <Grid.Column width={10}>
+            <Segment
+              className='desktop-index'
+              raised
+            >
+              {this.props.stories.map(story =>
+                <Link key={story.id} to ={{
+                    pathname: `/stories/${story.id}`,
+                    state: {
+                        reddit: this.props.reddit,
+                        storyId: story.id
+                }}}> <Segment
+                        style={style}
+                  > {story.title}
+                  </Segment>
+                </Link>
+              )}
+            </Segment>
+          </Grid.Column>
+          <Grid.Column width={3}></Grid.Column>
+        </Grid>
       </Container>
     )
   }
