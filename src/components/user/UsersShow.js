3432f780ddbe496a95b81e9e01a9e926899745fb
Siddant <siddantgurung@hotmail.co.uk>
2019-02-28 10:00:45 +0000

    user show layout done

--- src/components/user/UsersShow.js
+++ src/components/user/UsersShow.js
@@ -1,1 +1,25 @@
 import React from 'react'
+import { Segment, Container, Grid, Image  } from 'semantic-ui-react'
+
+
+class UsersShow extends React.Component{
+
+
+
+  render(){
+    return(
+      <Container>
+        <Grid columns={1} stackable textAlign='center'>
+          <Grid.Column width={8} >
+            <Image src='https://react.semantic-ui.com/images/avatar/large/patrick.png' size='medium' circular centered/>
+          </Grid.Column>
+        </Grid>
+
+      </Container>
+
+    )
+  }
+}
+
+
+export default UsersShow
