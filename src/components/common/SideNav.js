da95177ddcfd621361935681b635514b0092452d
Alessandro Lepri <alex.lepri84@gmail.com>
2019-02-28 13:48:24 +0000

    Changed sidebar

--- src/components/common/SideNav.js
+++ src/components/common/SideNav.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Icon, Sidebar, Menu } from 'semantic-ui-react'
+import { Icon, Sidebar, Menu, Divider } from 'semantic-ui-react'
 
 
 const SideNav = ({handleSidebarHide, visible}) => {
@@ -13,35 +13,59 @@ const SideNav = ({handleSidebarHide, visible}) => {
       vertical
       visible={visible}
       width='thin'
-    >
+    ><Divider hidden />
+
       <Menu.Item as='a'>
         <Icon name='home' />
         Home
       </Menu.Item>
+
+      <Menu.Item as='a'>
+        <Icon name='add user' />
+        Sign Up
+      </Menu.Item>
+
+      <Menu.Item as='a'>
+        <Icon name='user' />
+        Login
+      </Menu.Item>
+
       <Menu.Item as='a'>
         <Icon name='book' />
         Library
       </Menu.Item>
+
       <Menu.Item as='a'>
         <Icon name='envelope' />
         Message
       </Menu.Item>
-      <Menu.Item as='a'>
-        <Icon name='cogs' />
-        Setting
-      </Menu.Item>
+
       <Menu.Item as='a'>
         <Icon name='pencil alternate' />
         Add New Story
       </Menu.Item>
+
       <Menu.Item as='a'>
         <Icon name='file alternate outline' />
         Reding List
       </Menu.Item>
+
+
+      <Menu.Item as='a'>
+        <Icon name='cogs' />
+        Setting
+      </Menu.Item>
+
       <Menu.Item as='a'>
         <Icon name='question circle outline' />
         About Us
       </Menu.Item>
+
+      <Menu.Item as='a'>
+        <Icon name='log out' />
+        Log Out
+      </Menu.Item>
+
     </Sidebar>
   )
 }
