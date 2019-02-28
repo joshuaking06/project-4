36d04db0818678bbbb71ceb9a4f199f5b524c2ed
Siddant <siddantgurung@hotmail.co.uk>
2019-02-28 13:56:29 +0000

    breaking the user page into componenets

---
+++ src/components/user/UsersDetail.js
@@ -0,0 +1,64 @@
+import React from 'react'
+import {Statistic, Grid, Image,Header,Divider, Button,Icon,Card  } from 'semantic-ui-react'
+const description = [
+  'Amy is a violinist with 2 years experience in the wedding industry.',
+  'She enjoys the outdoors and currently resides in upstate New York.'
+].join(' ')
+const UsersDetail = () => {
+
+  return(
+
+    <Grid columns={1} stackable textAlign='center'>
+      <Grid.Column width={8}  >
+        <Header as='h1' icon textAlign='center'>
+          <Header.Content>User name</Header.Content>
+        </Header>
+
+
+        <Image src='https://react.semantic-ui.com/images/avatar/large/patrick.png' size='medium' circular centered/>
+
+        <Divider  hidden />
+        <Button primary className='detail'><Icon name='add user'/>Follow</Button>
+        <Button primary className='detail'> <Icon name='comments'/>Message User</Button>
+
+        <Divider hidden />
+        {/*
+                    <Button positive className='detail'> <Icon name='check circle'/>Following</Button>
+
+                  */}
+        <Divider hidden />
+
+        <Grid columns={3} stackable textAlign='center'>
+          <Statistic label='followers' value='5,550' />
+          <Statistic label='followings' value='5,550' />
+          <Statistic label='posts' value='5,550' />
+        </Grid>
+
+        <Divider  />
+
+
+        <Card className='users-profile-card' style={{width: 'auto'}}>
+          <Card.Content header='About Amy' />
+          <Card.Meta content='Horror, Sci-fi' />
+          <Card.Content description={description} />
+          <Card.Content extra >
+            <a>
+              <Icon name='comment outline' />
+                        22 comments
+            </a>
+            <a>
+              <Icon name='heart outline' />
+                        22 likes
+            </a>
+          </Card.Content>
+        </Card>
+
+
+      </Grid.Column>
+    </Grid>
+
+  )
+
+}
+
+export default UsersDetail
--- src/components/user/UsersShow.js
+++ src/components/user/UsersShow.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import axios from 'axios'
-import { Segment,Statistic, Container, Grid, Image,Header,Divider, Button,Icon,Card  } from 'semantic-ui-react'
+import { Segment, Container } from 'semantic-ui-react'
 
-const description = [
-  'Amy is a violinist with 2 years experience in the wedding industry.',
-  'She enjoys the outdoors and currently resides in upstate New York.'
-].join(' ')
+import UsersDetail from './UsersDetail'
+
 
 class UsersShow extends React.Component{
 
@@ -25,105 +23,7 @@ class UsersShow extends React.Component{
     return(
       <Container>
         <Segment>
-
-          <Grid columns={1} stackable textAlign='center'>
-            <Grid.Column width={8}  >
-              <Header as='h1' icon textAlign='center'>
-                <Header.Content>User name</Header.Content>
-              </Header>
-
-
-              <Image src='https://react.semantic-ui.com/images/avatar/large/patrick.png' size='medium' circular centered/>
-
-              <Divider  hidden />
-              <Button primary className='detail'><Icon name='add user'/>Follow</Button>
-              <Button primary className='detail'> <Icon name='comments'/>Message User</Button>
-
-              <Divider hidden />
-
-              <Button positive className='detail'> <Icon name='check circle'/>Following</Button>
-              <Divider hidden />
-
-              <Grid columns={3} stackable textAlign='center'>
-                <Statistic label='followers' value='5,550' />
-                <Statistic label='followings' value='5,550' />
-                <Statistic label='posts' value='5,550' />
-              </Grid>
-
-              <Divider  />
-
-              <Card className='users-profile-card' style={{width: 'auto'}}>
-                <Card.Content header='About Amy' />
-                <Card.Meta content='Horror, Sci-fi' />
-                <Card.Content description={description} />
-                <Card.Content extra >
-                  <a>
-                    <Icon name='comment outline' />
-                    22 comments
-                  </a>
-                  <a>
-                    <Icon name='heart outline' />
-                    22 likes
-                  </a>
-                </Card.Content>
-              </Card>
-
-
-              <Card className='users-profile-card' style={{width: 'auto'}}>
-                <Card.Content header='About Amy' />
-                <Card.Meta content='Horror, Sci-fi' />
-                <Card.Content description={description} />
-                <Card.Content extra >
-                  <a>
-                    <Icon name='comment outline' />
-                    22 comments
-                  </a>
-                  <a>
-                    <Icon name='heart outline' />
-                    22 likes
-                  </a>
-                </Card.Content>
-              </Card>
-
-
-              <Card className='users-profile-card' style={{width: 'auto'}}>
-                <Card.Content header='About Amy' />
-                <Card.Meta content='Horror, Sci-fi' />
-                <Card.Content description={description} />
-                <Card.Content extra >
-                  <a>
-                    <Icon name='comment outline' />
-                    22 comments
-                  </a>
-                  <a>
-                    <Icon name='heart outline' />
-                    22 likes
-                  </a>
-                </Card.Content>
-              </Card>
-
-
-
-
-              <Card className='users-profile-card' style={{width: 'auto'}}>
-                <Card.Content header='About Amy' />
-                <Card.Meta content='Horror, Sci-fi' />
-                <Card.Content description={description} />
-                <Card.Content extra >
-                  <a>
-                    <Icon name='comment outline' />
-                    22 comments
-                  </a>
-                  <a>
-                    <Icon name='heart outline' />
-                    22 likes
-                  </a>
-                </Card.Content>
-              </Card>
-
-
-            </Grid.Column>
-          </Grid>
+          <UsersDetail />
         </Segment>
       </Container>
 
