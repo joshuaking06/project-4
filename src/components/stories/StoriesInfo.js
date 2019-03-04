5e942a1cfd721b5b929df53ee8ec8b1503a5abd9
joshuaking06 <joshua.king0692@gmail.com>
2019-03-04 10:13:41 +0000

    displaying basic info on info page, no comments yet

--- src/components/stories/StoriesInfo.js
+++ src/components/stories/StoriesInfo.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Segment, Container, Divider, Button, Input, Feed } from 'semantic-ui-react'
+import { Segment, Container, Divider, Button, Input, Feed, Header } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
 import Settings from '../../lib/Settings'
 import Auth from '../../lib/Auth'
@@ -31,11 +31,29 @@ class StoriesInfo extends React.Component{
     console.log(this.state)
     const { nightMode } = this.state
     if(!this.state.data) return null
+    const { data } = this.state
     return(
-      <Container>
-        <Segment inverted={nightMode}>
-
-        </Segment>
+      <Container textAlign='center'>
+        <Segment.Group >
+          <Segment inverted={nightMode}><Header as='h2'> {data.title} </Header> </Segment>
+          <Segment.Group>
+            <Segment inverted={nightMode}><strong>Genre:</strong> {data.genre}</Segment>
+            <Segment inverted={nightMode}><strong>Description:</strong> {data.description}</Segment>
+            <Segment inverted={nightMode}> <strong>Posted:</strong> {data.created_at}</Segment>
+            <Segment inverted={nightMode}> <strong>Last Updated:</strong> {data.updated_at}</Segment>
+          </Segment.Group>
+          <Divider />
+          <Segment inverted={nightMode}><Header as='h2'> Author Info </Header> </Segment>
+          <Segment.Group>
+            <Segment inverted={nightMode}>
+              <strong>Username: </strong>
+              <Link to={`/users/${data.creator.id}`}>
+                {data.creator.username}
+              </Link>
+            </Segment>
+            <Segment inverted={nightMode}><strong>Followers:</strong> {data.creator.followers.length}</Segment>
+          </Segment.Group>
+        </Segment.Group>
       </Container>
     )
   }
