c1470c1a52a1eae3df9b8659a2b837fd7c557c57
Siddant <siddantgurung@hotmail.co.uk>
2019-03-04 10:03:01 +0000

    testing to see how the semantic ui works

---
+++ test/components/test_spec.js
@@ -0,0 +1,5 @@
+/* global describe, it, beforeEach */
+import React from 'react'
+import { expect } from 'chai'
+import { shallow } from 'enzyme'
+import UsersDetail from '../../src/components/UsersDetail'
