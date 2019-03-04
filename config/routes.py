782fff17c7c9c80a9895e4457bdd374705d3a74d
joshuaking06 <joshua.king0692@gmail.com>
2019-03-04 16:07:15 +0000

    built

--- config/routes.py
+++ config/routes.py
@@ -7,10 +7,12 @@ app.register_blueprint(stories.api, url_prefix='/api')
 app.register_blueprint(reddit.api, url_prefix='/api')
 app.register_blueprint(giphy.api, url_prefix='/api')
 
-@app.route('/', defaults={'path': ''}) #homepage
-@app.route('/<path:path>') #any other path
-def catch_all(path):
-    if os.path.isfile('/dist' + path):
-        return app.send_static_file(path) #if path is file send it back
 
-    return app.send_static_file('index.html') # otherwise send back the index html file
+@app.route('/', defaults={'path': ''}) # homepage
+@app.route('/<path:path>') # any other path
+def catch_all(path):
+
+    if os.path.isfile('dist/' + path): # if path is a file, send it back
+        return app.send_static_file(path)
+
+    return app.send_static_file('index.html') # otherwise send back the index.html file
