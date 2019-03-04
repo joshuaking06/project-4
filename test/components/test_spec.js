5993733e2da2ef012532a0ef2d5124d512f8a965
Siddant <siddantgurung@hotmail.co.uk>
2019-03-04 10:10:05 +0000

    data set up complete

--- test/components/test_spec.js
+++ test/components/test_spec.js
@@ -3,3 +3,35 @@ import React from 'react'
 import { expect } from 'chai'
 import { shallow } from 'enzyme'
 import UsersDetail from '../../src/components/UsersDetail'
+
+
+describe('Semantic UI tests', () => {
+  let wrapper
+  //usersDetail, handleUnfollowEvent, handleFollowEvent, handleUsersMessagingEvent
+  beforeEach(done => {
+    const usersDetail = {
+      _id: 1,
+      username: 'SiddantGurung',
+      followers: [],
+      following: [
+        {
+          'id': 3,
+          'usernam': 'joshuaking06'
+        }
+      ],
+      stories_written: [
+        {
+          'description': 'a short story about inter',
+          'genre': 'fantasy',
+          'id': 1,
+          'title': 'inter'
+        }
+      ]
+    }
+    wrapper = shallow(<UsersDetail usersDetail={usersDetail} />)
+    done()
+  })
+
+
+
+})
