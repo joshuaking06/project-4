8eaa4f88ebcbc0d81b33edf69a8ae8e2c5cb56a4
Siddant <siddantgurung@hotmail.co.uk>
2019-03-04 21:00:28 +0000

    message show route done

--- config/routes.py
+++ config/routes.py
@@ -1,11 +1,12 @@
 import os
 from app import app
-from controllers import auth, stories, reddit, giphy
+from controllers import auth, stories, reddit, giphy, message
 
 app.register_blueprint(auth.api, url_prefix='/api')
 app.register_blueprint(stories.api, url_prefix='/api')
 app.register_blueprint(reddit.api, url_prefix='/api')
 app.register_blueprint(giphy.api, url_prefix='/api')
+app.register_blueprint(message.api, url_prefix='/api')
 
 
 @app.route('/', defaults={'path': ''}) # homepage
---
+++ controllers/message.py
@@ -0,0 +1,17 @@
+from flask import Blueprint, request, jsonify, g
+from models.story import Story, StorySchema
+from lib.secure_route import secure_route
+from models.message import Message, MessageSchema
+
+api = Blueprint('message', __name__)
+
+
+message_schema = MessageSchema()
+
+
+# === message show ===
+@api.route('/messages/<int:message_id>', methods=['GET'])
+@secure_route
+def show(message_id):
+    message = Message.query.get(message_id)
+    return message_schema.jsonify(message)
