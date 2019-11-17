b2b70e306805e7bb6ed8eca49ae8045ddae45722
Siddant <siddantgurung@hotmail.co.uk>
2019-11-17 00:17:21 +0000

    updated enginee property in package.json

--- src/components/common/Home.js
+++ src/components/common/Home.js
@@ -1,34 +1,31 @@
-import React from 'react'
-import { Header, Container } from 'semantic-ui-react'
-
+import React from "react";
+import { Header, Container } from "semantic-ui-react";
 
 class Home extends React.Component {
   constructor() {
-    super()
+    super();
 
     this.state = {
       width: window.innerWidth
-
-    }
+    };
   }
 
   render() {
-
     return (
-
       <div>
-        <Header className='background'>
-
-          <Container className='homepage-margin'>
-            <Header as='h1' className='heading read-me'>READ-ME
+        <Header className="background">
+          <Container className="homepage-margin">
+            <Header as="h1" className="heading read-me">
+              READ-ME
             </Header>
-            <Header as="h4" className='heading2'>Share your short stories and join our readers community Testing Deploy
+            <Header as="h4" className="heading2">
+              Share your short stories and join our readers community
             </Header>
           </Container>
         </Header>
       </div>
-    )
+    );
   }
 }
 
-export default Home
+export default Home;
